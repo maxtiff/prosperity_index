b4e381c85bb5cf7d66e114a1e0fa2b3dfbf1e69a
Travis <travis.j.may@stls.frb.org>
2017-01-18 17:00:47 -0600

    sort data

--- fbi_crime_stats/scripts/fbi.py
+++ fbi_crime_stats/scripts/fbi.py
@@ -68,11 +68,24 @@ states = pd.read_table('..\\state_fips.txt',dtype=str)
 state_frame = pd.merge(states, abr, on='state')
 counties = pd.read_table('..\\national_county.txt',dtype=str,sep=';')
 
+keep = ['State','County','Violent\ncrime','Property\ncrime','date']
+
 # Import data
 data_dir = os.getcwd() + '\\data\\'
 
 test = pd.read_excel(data_dir + 'crime_15.xls',skiprows={0,1},header=2,skip_footer=8)
+test['State'].replace(to_replace='\s\-\s\w+\sCounties', value='',inplace=True,regex=True)
+test = test.fillna(method='ffill')
+test['date']='2015'
+test=test.filter(keep,axis=1)
+test = pd.merge(test,state_frame,left_on='State',right_on='name')
+test['County'].replace(to_replace='County Police Department|County Unified Police Department|Public Safety|Police Department|\d$', value='',inplace=True,regex=True)
+test['locale'] = test['County'] + ', ' + test['state']
+test.drop(['State', 'County','state','name'], axis=1, inplace=True)
+counties['test'] = counties['county'].apply(lambda x: difflib.get_close_matches(x, test['locale'])[:1] or [None])[0]
 
 test1 = pd.read_excel(data_dir + 'crime_14.xls',skiprows={0,1},header=2,skip_footer=8)
 
-test2 = pd.read_table(data_dir + 'crime_05.txt',skiprows={0,1},header=2)
+test2 = pd.read_table(data_dir + 'crime_05.txt',skiprows={0,1},header=2,skip_footer=6)
+
+test3 = pd.read_excel(data_dir + 'crime_05.xls',skiprows={0,1})
