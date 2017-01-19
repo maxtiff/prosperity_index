8afa57efdece317dad278389f3e9b15816b3a661
Travis <travis.j.may@stls.frb.org>
2017-01-19 10:11:16 -0600

    merge data frames

--- fbi_crime_stats/scripts/fbi.py
+++ fbi_crime_stats/scripts/fbi.py
@@ -68,10 +68,13 @@ states = pd.read_table('..\\state_fips.txt',dtype=str)
 state_frame = pd.merge(states, abr, on='state')
 counties = pd.read_table('..\\national_county.txt',dtype=str,sep=';')
 
-keep = ['State','County','Violent\ncrime','Property\ncrime','date']
-keep_alt = ['State','County','Violent \ncrime','Property \ncrime','date']
-keep_alt_2 = ['State','County','Violent Crime','Property Crime','date']
-keep_alt_3 = ['State','County','Violent crime','Property crime','date']
+keep = 'State|County|Violent|Property|date'
+names = ['violent','property','date','locale','fips','crime']
+# keep = ['State','County','Violent\ncrime','Property\ncrime','date']
+# keep_alt = ['State','County','Violent \ncrime','Property \ncrime','date']
+# keep_alt_2 = ['State','County','Violent Crime','Property Crime','date']
+# keep_alt_3 = ['State','County','Violent crime','Property crime','date']
+
 # Import data
 data_dir = os.getcwd() + '\\data\\'
 
@@ -88,12 +91,14 @@ crime_2015.drop(['State','County','state','name'], axis=1, inplace=True)
 crime_2015['locale'] = crime_2015['locale'].apply(lambda x: (dl.get_close_matches(x, counties['county'])[:1] or [None])[0])
 crime_2015=pd.merge(crime_2015,counties,left_on='locale',right_on='county')
 crime_2015['crime'] = crime_2015.sum(axis=1)
+crime_2015=crime_2015.filter(regex=('Violent|Property|date|fips_y|crime|locale'),axis=1)
+crime_2015.columns=[names]
 
 crime_2014= pd.read_excel(data_dir + 'crime_14.xls',skiprows={0,1},header=2,skip_footer=8)
 crime_2014['State'].replace(to_replace='\s\-\s\w+\sCounties|\d$', value='',inplace=True,regex=True)
 crime_2014= crime_2014.fillna(method='ffill')
 crime_2014['date']='2014'
-crime_2014=crime_2014.filter(keep,axis=1)
+crime_2014=crime_2014.filter(regex=keep,axis=1)
 crime_2014=pd.merge(crime_2014,state_frame,left_on='State',right_on='name')
 crime_2014['County'].replace(to_replace='County Police Department|County Unified Police Department|Public Safety|Police Department|\d$', value='',inplace=True,regex=True)
 crime_2014['locale'] = crime_2014['County'] + ', ' + crime_2014['state']
@@ -102,12 +107,14 @@ crime_2014['locale'] = crime_2014['locale'].apply(lambda x: (dl.get_close_matche
         'county'])[:1] or [None])[0])
 crime_2014=pd.merge(crime_2014,counties,left_on='locale',right_on='county')
 crime_2014['crime'] = crime_2014.sum(axis=1)
+crime_2014=crime_2014.filter(regex=('Violent|Property|date|fips_y|crime|locale'),axis=1)
+crime_2014.columns=[names]
 
 crime_2013= pd.read_excel(data_dir + 'crime_13.xls',skiprows={0,1},header=2,skip_footer=7)
 crime_2013['State'].replace(to_replace='\s\-\s\w+\sCounties|\d$', value='',inplace=True,regex=True)
 crime_2013= crime_2013.fillna(method='ffill')
 crime_2013['date']='2013'
-crime_2013=crime_2013.filter(keep,axis=1)
+crime_2013=crime_2013.filter(regex=keep,axis=1)
 crime_2013=pd.merge(crime_2013,state_frame,left_on='State',right_on='name')
 crime_2013['County'].replace(to_replace='County Police Department|County Unified Police Department|Public Safety|Police Department|\d$', value='',inplace=True,regex=True)
 crime_2013['locale'] = crime_2013['County'] + ', ' + crime_2013['state']
@@ -116,48 +123,56 @@ crime_2013['locale'] = crime_2013['locale'].apply(lambda x: (dl.get_close_matche
         'county'])[:1] or [None])[0])
 crime_2013=pd.merge(crime_2013,counties,left_on='locale',right_on='county')
 crime_2013['crime'] = crime_2013.sum(axis=1)
+crime_2013=crime_2013.filter(regex=('Violent|Property|date|fips_y|crime|locale'),axis=1)
+crime_2013columns=[names]
 
 crime_2012= pd.read_excel(data_dir + 'crime_12.xls',skiprows={0,1},header=2,skip_footer=7)
 crime_2012['State'].replace(to_replace='\s\-\s\w+\sCounties|\d$', value='',inplace=True,regex=True)
 crime_2012= crime_2012.fillna(method='ffill')
 crime_2012['date']='2012'
-crime_2012=crime_2012.filter(keep_alt,axis=1)
+crime_2012=crime_2012.filter(regex=keep,axis=1)
 crime_2012=pd.merge(crime_2012,state_frame,left_on='State',right_on='name')
-crime_2012['County'].replace(to_replace='County Police Department|County Unified Police Department|Public Safety|Police Department|\d$', value='',inplace=True,regex=True)
+crime_2012['County'].replace(to_replace='\d|County Police Department|County Unified Police Department|Public Safety|Police Department|\d+|\n$', value='',inplace=True,regex=True)
 crime_2012['locale'] = crime_2012['County'] + ', ' + crime_2012['state']
 crime_2012.drop(['State','County','state','name'], axis=1, inplace=True)
 crime_2012['locale'] = crime_2012['locale'].apply(lambda x: (dl.get_close_matches(x, counties[\
         'county'])[:1] or [None])[0])
 crime_2012=pd.merge(crime_2012,counties,left_on='locale',right_on='county')
 crime_2012['crime'] = crime_2012.sum(axis=1)
+crime_2012=crime_2012.filter(regex=('Violent|Property|date|fips_y|crime|locale'),axis=1)
+crime_2012.columns=[names]
 
 crime_2011= pd.read_excel(data_dir + 'crime_11.xls',skiprows={0,1},header=2,skip_footer=7)
 crime_2011['State'].replace(to_replace='\s\-\s+\w+\sCounties|\d$', value='',inplace=True,regex=True)
 crime_2011= crime_2011.fillna(method='ffill')
 crime_2011['date']='2011'
-crime_2011=crime_2011.filter(keep_alt,axis=1)
+crime_2011=crime_2011.filter(regex=keep,axis=1)
 crime_2011=pd.merge(crime_2011,state_frame,left_on='State',right_on='name')
-crime_2011['County'].replace(to_replace='County Police Department|County Unified Police Department|Public Safety|Police Department|\d$', value='',inplace=True,regex=True)
+crime_2011['County'].replace(to_replace='\d|County Police Department|County Unified Police Department|Public Safety|Police Department|\d+|\n$', value='',inplace=True,regex=True)
 crime_2011['locale'] = crime_2011['County'] + ', ' + crime_2011['state']
 crime_2011.drop(['State','County','state','name'], axis=1, inplace=True)
 crime_2011['locale'] = crime_2011['locale'].apply(lambda x: (dl.get_close_matches(x, counties[\
         'county'])[:1] or [None])[0])
 crime_2011=pd.merge(crime_2011,counties,left_on='locale',right_on='county')
 crime_2011['crime'] = crime_2011.sum(axis=1)
+crime_2011=crime_2011.filter(regex=('Violent|Property|date|fips_y|crime|locale'),axis=1)
+crime_2011.columns=[names]
 
 crime_2010= pd.read_excel(data_dir + 'crime_10.xls',skiprows={0,1},header=2,skip_footer=7)
 crime_2010['State'].replace(to_replace='\s\-\s\w+\sCounties|\d|\n$', value='',inplace=True,regex=True)
 crime_2010= crime_2010.fillna(method='ffill')
 crime_2010['date']='2010'
-crime_2010=crime_2010.filter(keep_alt,axis=1)
+crime_2010=crime_2010.filter(regex=keep,axis=1)
 crime_2010=pd.merge(crime_2010,state_frame,left_on='State',right_on='name')
-crime_2010['County'].replace(to_replace='County Police Department|County Unified Police Department|Public Safety|Police Department|\d$', value='',inplace=True,regex=True)
+crime_2010['County'].replace(to_replace='\d|County Police Department|County Unified Police Department|Public Safety|Police Department|\d|\n$', value='',inplace=True,regex=True)
 crime_2010['locale'] = crime_2010['County'] + ', ' + crime_2010['state']
 crime_2010.drop(['State','County','state','name'], axis=1, inplace=True)
 crime_2010['locale'] = crime_2010['locale'].apply(lambda x: (dl.get_close_matches(x, counties[\
         'county'])[:1] or [None])[0])
 crime_2010=pd.merge(crime_2010,counties,left_on='locale',right_on='county')
 crime_2010['crime'] = crime_2010.sum(axis=1)
+crime_2010=crime_2010.filter(regex=('Violent|Property|date|fips_y|crime|locale'),axis=1)
+crime_2010.columns=[names]
 
 crime_2009= pd.read_excel(data_dir + 'crime_09.xls',skiprows={0,1},header=2,skip_footer=7)
 crime_2009['State'].replace(to_replace='\s\-\s\w+\sCounties|\d$', value='',inplace=True,regex=True)
@@ -172,6 +187,8 @@ crime_2009['locale'] = crime_2009['locale'].apply(lambda x: (dl.get_close_matche
         'county'])[:1] or [None])[0])
 crime_2009=pd.merge(crime_2009,counties,left_on='locale',right_on='county')
 crime_2009['crime'] = crime_2009.sum(axis=1)
+crime_2009=crime_2009.filter(regex=('Violent|Property|date|fips_y|crime|locale'),axis=1)
+crime_2009.columns=[names]
 
 crime_2008= pd.read_excel(data_dir + 'crime_08.xls',skiprows={0,1},header=2,skip_footer=7)
 crime_2008['State'].replace(to_replace='\-\w+\sCounties|[^\x00-\x7F]\w+\sCounties|\d|\s$',value='',inplace=True,regex=True)
@@ -186,6 +203,8 @@ crime_2008['locale'] = crime_2008['locale'].apply(lambda x: (dl.get_close_matche
         'county'])[:1] or [None])[0])
 crime_2008=pd.merge(crime_2008,counties,left_on='locale',right_on='county')
 crime_2008['crime'] = crime_2008.sum(axis=1)
+crime_2008=crime_2008.filter(regex=('Violent|Property|date|fips_y|crime|locale'),axis=1)
+crime_2008.columns=[names]
 
 crime_2007= pd.read_excel(data_dir + 'crime_07.xls',skiprows={0,1},header=2,skip_footer=7)
 crime_2007['State'].replace(to_replace='\-\w+\sCounties|[^\x00-\x7F]\w+\sCounties|\d|\s$',value='',inplace=True,regex=True)
@@ -200,6 +219,8 @@ crime_2007['locale'] = crime_2007['locale'].apply(lambda x: (dl.get_close_matche
         'county'])[:1] or [None])[0])
 crime_2007=pd.merge(crime_2007,counties,left_on='locale',right_on='county')
 crime_2007['crime'] = crime_2007.sum(axis=1)
+crime_2007=crime_2007.filter(regex=('Violent|Property|date|fips_y|crime|locale'),axis=1)
+crime_2007.columns=[names]
 
 crime_2006= pd.read_excel(data_dir + 'crime_06.xls',skiprows={0,1},header=2,skip_footer=8)
 crime_2006['State'].replace(to_replace='\-\w+\sCounties|[^\x00-\x7F]\w+\sCounties|\d|\s$',value='',inplace=True,regex=True)
@@ -214,6 +235,8 @@ crime_2006['locale'] = crime_2006['locale'].apply(lambda x: (dl.get_close_matche
         'county'])[:1] or [None])[0])
 crime_2006=pd.merge(crime_2006,counties,left_on='locale',right_on='county')
 crime_2006['crime'] = crime_2006.sum(axis=1)
+crime_2006=crime_2006.filter(regex=('Violent|Property|date|fips_y|crime|locale'),axis=1)
+crime_2006.columns=[names]
 
 crime_2005= pd.read_excel(data_dir + 'crime_05.xls',skiprows={0,1},header=2,skip_footer=8)
 crime_2005['State'].replace(to_replace='\-\w+\sCounties|[^\x00-\x7F]\w+\sCounties|\d|\s$',value='',inplace=True,regex=True)
@@ -228,6 +251,14 @@ crime_2005['locale'] = crime_2005['locale'].apply(lambda x: (dl.get_close_matche
         'county'])[:1] or [None])[0])
 crime_2005=pd.merge(crime_2005,counties,left_on='locale',right_on='county')
 crime_2005['crime'] = crime_2005.sum(axis=1)
+crime_2005=crime_2005.filter(regex=('Violent|Property|date|fips_y|crime|locale'),axis=1)
+crime_2005.columns=[names]
 
-dfs = ['crime_2015','crime_2014','crime_2013','crime_2012','crime_2011','crime_2010',
-       'crime_2009','crime_2008','crime_2007','crime_2006','crime_2005']
+dfs = [crime_2015,crime_2014,crime_2013,crime_2012,crime_2011,crime_2010,crime_2009,crime_2008,crime_2007,crime_2006,crime_2005]
+
+def multi_ordered_merge(lst_dfs):
+    reduce_func = lambda left,right: pd.ordered_merge(left, right)
+
+    return ft.reduce(reduce_func, lst_dfs)
+
+df = multi_ordered_merge(dfs)
