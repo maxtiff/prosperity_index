bc1bc237c5bd3bc258539262111e49cd01992b69
Travis <travis.j.may@stls.frb.org>
2017-01-24 16:00:38 -0600

    Import and clean tract data

---
+++ racial_segregation/scripts/racial_seg.py
@@ -0,0 +1,18 @@
+import pandas as pd, os, sys, functools as ft, pycurl as pyc, datetime as dt, re, numpy as np
+pd.options.mode.chained_assignment = None  # default='warn'
+
+data_dir = os.getcwd() + '\\data\\'
+
+keep_tract = ['GEO.id2','HD01_VD01','HD01_VD03','HD01_VD04','HD01_VD06','HD01_VD12']
+tract_names = ['fips','total','white','black','asian','hispanic']
+
+tract_09 = pd.read_csv(data_dir + 'race_tract_09.csv',encoding='windows-1252',skiprows={1})
+tract_09 = tract_09.filter(keep_tract,axis=1)
+tract_09.columns = [tract_names]
+tract_09['fips']=tract_09['fips'].astype(str)
+tract_09['tract']=tract_09.fips.str.extract('(?P<tract>\d{5}$)')
+tract_09['fips']=tract_09['fips'].str[:-6].astype(np.int64)
+tract_09['fips']=tract_09['fips'].apply(lambda x:"%06d" % (x,))
+
+county_09 =
+
