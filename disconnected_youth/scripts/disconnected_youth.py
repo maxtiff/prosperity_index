b49471cdef979ba40e25137f300e3194c9f732f1
Travis <travis.j.may@stls.frb.org>
2017-01-25 15:12:41 -0600

    Clean up

--- disconnected_youth/scripts/disconnected_youth.py
+++ disconnected_youth/scripts/disconnected_youth.py
@@ -1,75 +1,37 @@
 import pandas as pd, os, sys, functools as ft, pycurl as pyc, datetime as dt, re, numpy as np
 pd.options.mode.chained_assignment = None  # default='warn'
 
-keep = ['GEO.id2','GEO.display-label','HD01_VD01','HD01_VD10','HD01_VD11','HD01_VD14','HD01_VD15','HD01_VD24',\
-        'HD01_VD25','HD01_VD28','HD01_VD29']
-
-regex = ('GEO.id2|GEO.display-label|disconnected_youth|date')
-
 data_dir = os.getcwd() + '\\data\\'
 
-youth_09 = pd.read_csv(data_dir+'disconnected_youth_09.csv',encoding='windows-1252',skiprows={1})
-youth_09 = youth_09.filter(keep,axis=1)
-youth_09['GEO.id2']=youth_09['GEO.id2'].apply(lambda x:"%06d" % (x,))
-youth_09['date'] = '2009'
-# youth_09['disconnected_youth']=((youth_09['HD01_VD10'] + youth_09['HD01_VD11'] + youth_09['HD01_VD14'] \
-#                                 +youth_09['HD01_VD15'] + youth_09['HD01_VD24'] + youth_09['HD01_VD25'] \
-#                                 +youth_09['HD01_VD28'] + youth_09['HD01_VD29'])/youth_09['HD01_VD01'])*100
-# youth_09 = youth_09.filter(regex=regex,axis=1)
+def dc_youth(dy_data,date):
 
-youth_10 = pd.read_csv(data_dir+'disconnected_youth_10.csv',encoding='windows-1252',skiprows={1})
-youth_10 = youth_10.filter(keep,axis=1)
-youth_10['GEO.id2']=youth_10['GEO.id2'].apply(lambda x:"%06d" % (x,))
-youth_10['date'] = '2010'
-# youth_10['disconnected_youth']=((youth_10['HD01_VD10'] + youth_10['HD01_VD11'] + youth_10['HD01_VD14'] \
-#                                 +youth_10['HD01_VD15'] + youth_10['HD01_VD24'] + youth_10['HD01_VD25'] \
-#                                 +youth_10['HD01_VD28'] + youth_10['HD01_VD29'])/youth_10['HD01_VD01'])*100
-# youth_10 = youth_10.filter(regex=regex,axis=1)
+    keep = ['GEO.id2','GEO.display-label','HD01_VD01','HD01_VD10','HD01_VD11','HD01_VD14','HD01_VD15','HD01_VD24',\
+            'HD01_VD25','HD01_VD28','HD01_VD29']
 
-youth_11 = pd.read_csv(data_dir+'disconnected_youth_11.csv',encoding='windows-1252',skiprows={1})
-youth_11 = youth_11.filter(keep,axis=1)
-youth_11['GEO.id2']=youth_11['GEO.id2'].apply(lambda x:"%06d" % (x,))
-youth_11['date'] = '2011'
-# youth_11['disconnected_youth']=((youth_11['HD01_VD10'] + youth_11['HD01_VD11'] + youth_11['HD01_VD14'] \
-#                                 +youth_11['HD01_VD15'] + youth_11['HD01_VD24'] + youth_11['HD01_VD25'] \
-#                                 +youth_11['HD01_VD28'] + youth_11['HD01_VD29'])/youth_11['HD01_VD01'])*100
-# youth_11 = youth_11.filter(regex=regex,axis=1)
+    df = pd.read_csv(dy_data,encoding='windows-1252',skiprows={1})
+    df = df.filter(keep,axis=1)
+    df['GEO.id2']=df['GEO.id2'].apply(lambda x:"%06d" % (x,))
+    df['date'] = date
 
-youth_12 = pd.read_csv(data_dir+'disconnected_youth_12.csv',encoding='windows-1252',skiprows={1})
-youth_12 = youth_12.filter(keep,axis=1)
-youth_12['GEO.id2']=youth_12['GEO.id2'].apply(lambda x:"%06d" % (x,))
-youth_12['date'] = '2012'
-# youth_12['disconnected_youth']=((youth_12['HD01_VD10'] + youth_12['HD01_VD11'] + youth_12['HD01_VD14'] \
-#                                 +youth_12['HD01_VD15'] + youth_12['HD01_VD24'] + youth_12['HD01_VD25'] \
-#                                 +youth_12['HD01_VD28'] + youth_12['HD01_VD29'])/youth_12['HD01_VD01'])*100
-# youth_12 = youth_12.filter(regex=regex,axis=1)
-
-youth_13 = pd.read_csv(data_dir+'disconnected_youth_13.csv',encoding='windows-1252',skiprows={1})
-youth_13 = youth_13.filter(keep,axis=1)
-youth_13['GEO.id2']=youth_13['GEO.id2'].apply(lambda x:"%06d" % (x,))
-youth_13['date'] = '2013'
-# youth_13['disconnected_youth']=((youth_13['HD01_VD10'] + youth_13['HD01_VD11'] + youth_13['HD01_VD14'] \
-#                                 +youth_13['HD01_VD15'] + youth_13['HD01_VD24'] + youth_13['HD01_VD25'] \
-#                                 +youth_13['HD01_VD28'] + youth_13['HD01_VD29'])/youth_13['HD01_VD01'])*100
-# youth_13 = youth_13.filter(regex=regex,axis=1)
-
-youth_14 = pd.read_csv(data_dir+'disconnected_youth_14.csv',encoding='windows-1252',skiprows={1})
-youth_14 = youth_14.filter(keep,axis=1)
-youth_14['GEO.id2']=youth_14['GEO.id2'].apply(lambda x:"%06d" % (x,))
-youth_14['date'] = '2014'
-# youth_14['disconnected_youth']=((youth_14['HD01_VD10'] + youth_14['HD01_VD11'] + youth_14['HD01_VD14'] \
-#                                 +youth_14['HD01_VD15'] + youth_14['HD01_VD24'] + youth_14['HD01_VD25'] \
-#                                 +youth_14['HD01_VD28'] + youth_14['HD01_VD29'])/youth_14['HD01_VD01'])*100
-# youth_14 = youth_14.filter(regex=regex,axis=1)
+    return df
 
 def multi_ordered_merge(lst_dfs):
     reduce_func = lambda left,right: pd.ordered_merge(left, right)
 
     return ft.reduce(reduce_func, lst_dfs)
 
+youth_09 = dc_youth(data_dir+'disconnected_youth_09.csv','2009')
+youth_10 = dc_youth(data_dir+'disconnected_youth_10.csv','2010')
+youth_11 = dc_youth(data_dir+'disconnected_youth_11.csv','2011')
+youth_12 = dc_youth(data_dir+'disconnected_youth_12.csv','2012')
+youth_13 = dc_youth(data_dir+'disconnected_youth_13.csv','2013')
+youth_14 = dc_youth(data_dir+'disconnected_youth_14.csv','2014')
+
 dfs = [youth_09,youth_10,youth_11,youth_12,youth_13,youth_14]
 
 df = multi_ordered_merge(dfs)
+
+regex = ('GEO.id2|GEO.display-label|disconnected_youth|date')
 
 df['disconnected_youth']=((df['HD01_VD10'] +df['HD01_VD11'] + df['HD01_VD14'] \
                                 +df['HD01_VD15'] + df['HD01_VD24'] + df['HD01_VD25'] \
