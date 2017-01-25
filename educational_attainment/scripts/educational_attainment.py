55ffdbdfeff32bcb72a66c8464f749f329c8fdd5
Travis <travis.j.may@stls.frb.org>
2017-01-25 16:22:13 -0600

    Add main function wrapper

--- educational_attainment/scripts/educational_attainment.py
+++ educational_attainment/scripts/educational_attainment.py
@@ -17,54 +17,58 @@ def multi_ordered_merge(lst_dfs):
 
     return ft.reduce(reduce_func, lst_dfs)
 
-keep_09 = ['GEO.id2','GEO.display-label','HC01_EST_VC11','HC01_EST_VC12','HC01_EST_VC13']
-keep = ['GEO.id2','GEO.display-label','HC01_EST_VC12','HC01_EST_VC13',\
-		   'HC01_EST_VC14']
-names = ['fips','county','assc','bach','grad']
+def main():
+    keep_09 = ['GEO.id2','GEO.display-label','HC01_EST_VC11','HC01_EST_VC12','HC01_EST_VC13']
+    keep = ['GEO.id2','GEO.display-label','HC01_EST_VC12','HC01_EST_VC13',\
+               'HC01_EST_VC14']
+    names = ['fips','county','assc','bach','grad']
 
-data_dir = os.getcwd()+'\\data\\'
+    data_dir = os.getcwd()+'\\data\\'
 
-edu_09 = edu_attain(data_dir+'educational_attainment_09.csv',keep_09,\
-					names,'2009')
-edu_10 = edu_attain(data_dir+'educational_attainment_10.csv',keep,\
-					names,'2010')
-edu_11 = edu_attain(data_dir+'educational_attainment_11.csv',keep,\
-					names,'2011')
-edu_12 = edu_attain(data_dir+'educational_attainment_11.csv',keep,\
-					names,'2012')
-edu_13 = edu_attain(data_dir+'educational_attainment_11.csv',keep,\
-					names,'2012')
-edu_14 = edu_attain(data_dir+'educational_attainment_11.csv',keep,\
-					names,'2014')
+    edu_09 = edu_attain(data_dir+'educational_attainment_09.csv',keep_09,\
+                        names,'2009')
+    edu_10 = edu_attain(data_dir+'educational_attainment_10.csv',keep,\
+                        names,'2010')
+    edu_11 = edu_attain(data_dir+'educational_attainment_11.csv',keep,\
+                        names,'2011')
+    edu_12 = edu_attain(data_dir+'educational_attainment_11.csv',keep,\
+                        names,'2012')
+    edu_13 = edu_attain(data_dir+'educational_attainment_11.csv',keep,\
+                        names,'2012')
+    edu_14 = edu_attain(data_dir+'educational_attainment_11.csv',keep,\
+                        names,'2014')
 
-dfs = [edu_09,edu_10,edu_11,edu_12,edu_13,edu_14]
+    dfs = [edu_09,edu_10,edu_11,edu_12,edu_13,edu_14]
 
-df = multi_ordered_merge(dfs)
+    df = multi_ordered_merge(dfs)
 
-# df.fips = df.fips.astype(int)
-df = df.sort_values(['fips','date'])
-df.fips = df.fips.astype(str)
-# levels = df['fips'].unique()
+    # df.fips = df.fips.astype(int)
+    df = df.sort_values(['fips','date'])
+    df.fips = df.fips.astype(str)
+    # levels = df['fips'].unique()
 
-for series in pd.unique(df['fips'].ravel()):
-	frame = df[df['fips'] == series]
-	frame.reset_index(inplace=True)
-	# frame = frame.sort_values(['date'])
-	frame.drop(['index'], axis=1, inplace=True)
-	frame['total_third'] = frame.sum(axis=1)
+    for series in pd.unique(df['fips'].ravel()):
+        frame = df[df['fips'] == series]
+        frame.reset_index(inplace=True)
+        # frame = frame.sort_values(['date'])
+        frame.drop(['index'], axis=1, inplace=True)
+        frame['total_third'] = frame.sum(axis=1)
 
-	for c in ['assc','bach','grad','total_third']:
-		output = frame[['date', c]]
-		# frame['date'] = dates
-		output.set_index('date', inplace=True)
-		series_id = 'HC01'
-		if c is 'assc':
-			series_id = series_id+ 'ASSOC' + series
-		elif c is 'bach':
-			series_id = series_id+ 'BACH' + series
-		elif c is 'grad':
-			series_id = series_id+ 'GRAD' + series
-		elif c is 'total_third':
-			series_id = series_id+ 'TOTAL'+ series
-		output.columns = [series_id]
-		output.to_csv('output\\' + series_id, sep='\t')
+        for c in ['assc','bach','grad','total_third']:
+            output = frame[['date', c]]
+            # frame['date'] = dates
+            output.set_index('date', inplace=True)
+            series_id = 'HC01'
+            if c is 'assc':
+                series_id = series_id+ 'ASSOC' + series
+            elif c is 'bach':
+                series_id = series_id+ 'BACH' + series
+            elif c is 'grad':
+                series_id = series_id+ 'GRAD' + series
+            elif c is 'total_third':
+                series_id = series_id+ 'TOTAL'+ series
+            output.columns = [series_id]
+            output.to_csv('output\\' + series_id, sep='\t')
+
+if __name__='__main__':
+    main()
