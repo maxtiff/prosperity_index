84f221b2c2c8e2f661e7b2e26871bf18a05888ca
Travis <travis.j.may@stls.frb.org>
2017-01-26 14:00:35 -0600

    clean up

--- income_inequality/scripts/income_inequality.py
+++ income_inequality/scripts/income_inequality.py
@@ -56,35 +56,7 @@ def main():
         frame.columns = [series_id]
         frame.to_csv('output\\' + series_id, sep='\t')
 
+    return df
 
 if __name__ == '__main__':
     main()
-# gini_10 = pd.read_csv(data_dir+'income_inequality_10.csv',encoding='windows-1252',skiprows={1})
-# gini_10 = gini_10.filter(keep,axis=1)
-# gini_10.columns=[names]
-# gini_10['fips']=gini_10['fips'].apply(lambda x:"%06d" % (x,))
-# gini_10['date'] = '2010'
-#
-# gini_11 = pd.read_csv(data_dir+'income_inequality_11.csv',encoding='windows-1252',skiprows={1})
-# gini_11 = gini_11.filter(keep,axis=1)
-# gini_11.columns=[names]
-# gini_11['fips']=gini_11['fips'].apply(lambda x:"%06d" % (x,))
-# gini_11['date'] = '2011'
-#
-# gini_12 = pd.read_csv(data_dir+'income_inequality_12.csv',encoding='windows-1252',skiprows={1})
-# gini_12 = gini_12.filter(keep,axis=1)
-# gini_12.columns=[names]
-# gini_12['fips']=gini_12['fips'].apply(lambda x:"%06d" % (x,))
-# gini_12['date'] = '2012'
-#
-# gini_13 = pd.read_csv(data_dir+'income_inequality_13.csv',encoding='windows-1252',skiprows={1})
-# gini_13 = gini_13.filter(keep,axis=1)
-# gini_13.columns=[names]
-# gini_13['fips']=gini_13['fips'].apply(lambda x:"%06d" % (x,))
-# gini_13['date'] = '2013'
-#
-# gini_14 = pd.read_csv(data_dir+'income_inequality_14.csv',encoding='windows-1252',skiprows={1})
-# gini_14 = gini_14.filter(keep,axis=1)
-# gini_14.columns=[names]
-# gini_14['fips']=gini_14['fips'].apply(lambda x:"%06d" % (x,))
-# gini_14['date'] = '2014'
