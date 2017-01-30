50106fa25f87ab17936883bffd95b75fdb73d591
Travis <travis.j.may@stls.frb.org>
2017-01-30 12:36:46 -0600

    remove metadata stuff for now

--- disconnected_youth/scripts/disconnected_youth.py
+++ disconnected_youth/scripts/disconnected_youth.py
@@ -34,47 +34,48 @@ def main():
     youth_12 = dc_youth(data_dir+'disconnected_youth_12.csv','2012')
     youth_13 = dc_youth(data_dir+'disconnected_youth_13.csv','2013')
     youth_14 = dc_youth(data_dir+'disconnected_youth_14.csv','2014')
+    youth_15 = dc_youth(data_dir +'disconnected_youth_15.csv','2015')
 
-    dfs = [youth_09,youth_10,youth_11,youth_12,youth_13,youth_14]
+    dfs = [youth_09,youth_10,youth_11,youth_12,youth_13,youth_14,youth_15]
 
     df = multi_ordered_merge(dfs)
 
     df = df.sort_values(['GEO.id2','date'])
 
     ### Metadata
-    md_names = ['series_id','title','season','frequency','units','keywords',\
-                'notes','period_description','growth_rates',\
-                'obs_vsd_use_release_date','valid_start_date','release_id']
-    fsr_names = ['fred_release_id','fred_series_id','official','valid_start_date']
-    cat_names = ['series_id','cat_id']
-
-    geo_md = pd.DataFrame(columns=md_names)
-    fred_md = pd.DataFrame(columns=md_names)
-    fsr_geo = pd.DataFrame(columns=fsr_names)
-    fsr = pd.DataFrame(columns=fsr_names)
-    fred_cat = pd.DataFrame(columns=cat_names)
-
-    season = 'Not Seasonally Adjusted'
-    freq = '5-years'
-    units = 'Percent'
-    keywords = ''
-    notes = 'Disconnected Youth represents the percentage of youth in a ' \
-            'county who are between the ages of 16 and 19 who are not ' \
-            'enrolled in school and who are unemployed or not in the labor ' \
-            'force.####The date of the data is the end of the 5-year period. ' \
-            'For example, a value dated 2015 represents data from 2010 to 2015.'
-    period = ''
-    g_rate = 'TRUE'
-    obs_vsd = 'TRUE'
-    vsd = '2017-01-27'
-    r_id = '408'
-
-
-    non_geo_fips = '002020|002110|002220|002230|002275|006075|008014|015003|042101'
-
-    non_geo_cats = {'002020':'27406','002110':'27412','002220':'27422',\
-                    '002230':'33516','002275':'33518','006075':'27559',\
-                    '008014':'32077','015003':'27889','042101':'29664'}
+    # md_names = ['series_id','title','season','frequency','units','keywords',\
+    #             'notes','period_description','growth_rates',\
+    #             'obs_vsd_use_release_date','valid_start_date','release_id']
+    # fsr_names = ['fred_release_id','fred_series_id','official','valid_start_date']
+    # cat_names = ['series_id','cat_id']
+    #
+    # geo_md = pd.DataFrame(columns=md_names)
+    # fred_md = pd.DataFrame(columns=md_names)
+    # fsr_geo = pd.DataFrame(columns=fsr_names)
+    # fsr = pd.DataFrame(columns=fsr_names)
+    # fred_cat = pd.DataFrame(columns=cat_names)
+    #
+    # season = 'Not Seasonally Adjusted'
+    # freq = '5-years'
+    # units = 'Percent'
+    # keywords = ''
+    # notes = 'Disconnected Youth represents the percentage of youth in a ' \
+    #         'county who are between the ages of 16 and 19 who are not ' \
+    #         'enrolled in school and who are unemployed or not in the labor ' \
+    #         'force.####The date of the data is the end of the 5-year period. ' \
+    #         'For example, a value dated 2015 represents data from 2010 to 2015.'
+    # period = ''
+    # g_rate = 'TRUE'
+    # obs_vsd = 'TRUE'
+    # vsd = '2017-01-27'
+    # r_id = '408'
+    #
+    #
+    # non_geo_fips = '002020|002110|002220|002230|002275|006075|008014|015003|042101'
+    #
+    # non_geo_cats = {'002020':'27406','002110':'27412','002220':'27422',\
+    #                 '002230':'33516','002275':'33518','006075':'27559',\
+    #                 '008014':'32077','015003':'27889','042101':'29664'}
     ###
 
     # Create output files
@@ -88,38 +89,38 @@ def main():
         frame.to_csv('output\\' + series_id, sep='\t')
 
         # Create metadata files
-        title = 'Disconnected Youth (5-year estimate) for ' + \
-                pd.unique(df[df['GEO.id2'] == l]['GEO.display-label'])[0]
-
-        if bool(re.search(non_geo_fips, l)):
-            row=pd.DataFrame(data=[[series_id, title, season,freq, units,keywords,\
-                                    notes, period, g_rate, obs_vsd, vsd, r_id]],\
-                             columns=md_names)
-            fred_md = fred_md.append(row)
-
-            row = pd.DataFrame(data=[[r_id,series_id,'TRUE',vsd]],columns=fsr_names)
-            fsr = fsr.append(row)
-
-            cat_id = non_geo_cats[l]
-            row = pd.DataFrame(data=[[series_id,cat_id]],columns=cat_names)
-            fred_cat = fred_cat.append(row)
-
-        else:
-            row = pd.DataFrame(data=[[series_id, title, season,freq, units,keywords,\
-                                      notes, period, g_rate, obs_vsd, vsd, r_id]],\
-                               columns=md_names)
-            geo_md = geo_md.append(row)
-
-            row = pd.DataFrame(data=[[r_id, series_id,'TRUE', vsd]],columns=fsr_names)
-            fsr_geo = fsr_geo.append(row)
-
-    # Write metadata files
-    geo_md.to_csv('fred_series_geo.txt',sep='\t',index=False)
-    fsr_geo.to_csv('fred_series_release_geo.txt',sep='\t',index=False)
-
-    fred_md.to_csv('fred_series.txt',sep='\t',index=False)
-    fsr.to_csv('fred_series_release.txt',sep='\t',index=False)
-    fred_cat.to_csv('fred_series_in_category.txt',sep='\t',index=False)
+    #     title = 'Disconnected Youth (5-year estimate) for ' + \
+    #             pd.unique(df[df['GEO.id2'] == l]['GEO.display-label'])[0]
+    #
+    #     if bool(re.search(non_geo_fips, l)):
+    #         row=pd.DataFrame(data=[[series_id, title, season,freq, units,keywords,\
+    #                                 notes, period, g_rate, obs_vsd, vsd, r_id]],\
+    #                          columns=md_names)
+    #         fred_md = fred_md.append(row)
+    #
+    #         row = pd.DataFrame(data=[[r_id,series_id,'TRUE',vsd]],columns=fsr_names)
+    #         fsr = fsr.append(row)
+    #
+    #         cat_id = non_geo_cats[l]
+    #         row = pd.DataFrame(data=[[series_id,cat_id]],columns=cat_names)
+    #         fred_cat = fred_cat.append(row)
+    #
+    #     else:
+    #         row = pd.DataFrame(data=[[series_id, title, season,freq, units,keywords,\
+    #                                   notes, period, g_rate, obs_vsd, vsd, r_id]],\
+    #                            columns=md_names)
+    #         geo_md = geo_md.append(row)
+    #
+    #         row = pd.DataFrame(data=[[r_id, series_id,'TRUE', vsd]],columns=fsr_names)
+    #         fsr_geo = fsr_geo.append(row)
+    #
+    # # Write metadata files
+    # geo_md.to_csv('fred_series_geo.txt',sep='\t',index=False)
+    # fsr_geo.to_csv('fred_series_release_geo.txt',sep='\t',index=False)
+    #
+    # fred_md.to_csv('fred_series.txt',sep='\t',index=False)
+    # fsr.to_csv('fred_series_release.txt',sep='\t',index=False)
+    # fred_cat.to_csv('fred_series_in_category.txt',sep='\t',index=False)
 
 
 if __name__=='__main__':
