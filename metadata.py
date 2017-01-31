e5bcb4b71f5f89f3f60dc3ece5a6018c4e23fcdf
Travis <travis.j.may@stls.frb.org>
2017-01-31 16:20:10 -0600

    metadata stuff

--- metadata.py
+++ metadata.py
@@ -1,4 +1,4 @@
-md_names = ['series_id', 'title', 'season', 'frequency', 'units',
+md_names = ['series_id', 'title', 'season', 'frequency', 'units',\
             'keywords','notes', 'period_description', 'growth_rates',\
             'obs_vsd_use_release_date', 'valid_start_date', 'release_id']
 fsr_names = ['fred_release_id', 'fred_series_id', 'official',
@@ -13,19 +13,15 @@ fred_cat = pd.DataFrame(columns=cat_names)
 titles =pd.DataFrame()
 
 season = 'Not Seasonally Adjusted'
-freq = '5-years'
-units = 'Percent'
+freq = 'Annual'
+units = 'Units'
 keywords = ''
-notes = 'Estimate of educational attainment using 5 years of data. '\
-        'For more information see Appendix 1 of the ACS General '\
-        'Handbook (http://www.census.gov/content/dam/Census/library/publications/2008/acs/ACSGeneralHandbook.pdf). '\
-        '####The date of the data is the end of the 5-year period. ' \
-        'For example, a value dated 2015 represents data from 2010 to 2015.'
+notes = ''
 period = ''
 g_rate = 'TRUE'
 obs_vsd = 'TRUE'
 vsd = '2017-01-27'
-r_id = '330'
+r_id = '148'
 
 non_geo_fips = '002020|002110|002220|002230|002275|006075|008014|015003|042101'
 
@@ -33,7 +29,7 @@ non_geo_cats = {'002020': '27406', '002110': '27412', '002220': '27422', \
                 '002230': '33516', '002275': '33518', '006075': '27559', \
                 '008014': '32077', '015003': '27889', '042101': '29664'}
 
-title = 'People 25 Years and Over Who Have Completed an Associate\'s Degree or Higher (5-year estimate) in ' + \
+title = 'New Private Housing Units Authorized by Building Permits for ' + \
         pd.unique(df[df['fips'] == series]['county'])[0]
 
 # Create metadata files
@@ -58,3 +54,14 @@ else:
 
     row = pd.DataFrame(data=[[r_id, series_id, 'TRUE', vsd]],columns=fsr_names)
     fsr_geo = fsr_geo.append(row)
+
+title = pd.DataFrame(data=[[title]])
+titles = titles.append(title)
+
+geo_md.to_csv('fred_series_geo.txt', sep='\t', index=False)
+fsr_geo.to_csv('fred_series_release_geo.txt', sep='\t', index=False)
+
+fred_md.to_csv('fred_series.txt', sep='\t', index=False)
+fsr.to_csv('fred_series_release.txt', sep='\t', index=False)
+fred_cat.to_csv('fred_series_in_category.txt', sep='\t', index=False)
+titles.to_csv('title.txt', sep='\t', index=False, header=False)
