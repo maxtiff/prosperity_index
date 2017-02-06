a00221dc78422edf08e57592eaa434ad7dd1d54d
Travis <travis.j.may@stls.frb.org>
2017-02-06 16:52:38 -0600

    Create fred metadata files

--- preventable_hospital_admissions/scripts/hospital_admissions.py
+++ preventable_hospital_admissions/scripts/hospital_admissions.py
@@ -16,7 +16,7 @@ def multi_ordered_merge(lst_dfs):
 
 def main():
     names = ['fips','rate']
-
+    counties = pd.read_table('..\\national_county.txt', dtype=str, sep=';')
     data_dir = os.getcwd() + '\\data\\'
 
     pc_08 = preventable_care(data_dir+'PC_County_rates_2008.xls',names,'2008')
@@ -31,10 +31,11 @@ def main():
 
     df = multi_ordered_merge(dfs)
 
+    df = pd.merge(df, counties, on='fips')
+
     df = df.sort_values(['fips','date'])
 
-    for l in pd.unique(df['fips'].ravel()):
-        series = l
+    for series in pd.unique(df['fips'].ravel()):
         frame = df[df['fips'] == series]
         series_id = 'DMPCRATE' + series
         frame.reset_index(inplace=True)
@@ -43,5 +44,78 @@ def main():
         frame.columns = [series_id]
         frame.to_csv('output\\' + series_id, sep='\t')
 
+
 if __name__=='__main__':
     main()
+
+    # md_names = ['series_id', 'title', 'season', 'frequency', 'units', \
+    #             'keywords', 'notes', 'period_description', 'growth_rates', \
+    #             'obs_vsd_use_release_date', 'valid_start_date', 'release_id']
+    # fsr_names = ['fred_release_id', 'fred_series_id', 'official',\
+    #              'valid_start_date']
+    # cat_names = ['series_id', 'cat_id']
+    #
+    # geo_md = pd.DataFrame(columns=md_names)
+    # fred_md = pd.DataFrame(columns=md_names)
+    # fsr_geo = pd.DataFrame(columns=fsr_names)
+    # fsr = pd.DataFrame(columns=fsr_names)
+    # fred_cat = pd.DataFrame(columns=cat_names)
+    # titles = pd.DataFrame()
+    #
+    # season = 'Not Seasonally Adjusted'
+    # freq = 'Annual'
+    # units = 'Rate'
+    # keywords = ''
+    # notes = 'The Dartmouth Atlas of Healthcare calculates preventable hospital '\
+    #         'admissions by considering the discharges for ambulatory care '\
+    #         'sensitive conditions per 1,000 medicare enrollees.'\
+    #         '####See http://www.dartmouthatlas.org/downloads/reports/Primary_care_report_090910.pdf for more information.'
+    # period = ''
+    # g_rate = 'TRUE'
+    # obs_vsd = 'TRUE'
+    # vsd = '2017-02-03'
+    # r_id = '417'
+    #
+    # non_geo_fips = '002020|002110|002220|002230|002275|006075|008014|015003|042101'
+    #
+    # non_geo_cats = {'002020': '27406', '002110': '27412', '002220': '27422', \
+    #                 '002230': '33516', '002275': '33518', '006075': '27559', \
+    #                 '008014': '32077', '015003': '27889', '042101': '29664'}
+
+    #     title = 'Rate of Preventable Hospital Admissions in ' + \
+    #             pd.unique(df[df['fips'] == series]['county'])[0]
+    #
+    #     # Create metadata files
+    #     if bool(re.search(non_geo_fips, series)):
+    #         row = pd.DataFrame(data=[
+    #             [series_id, title, season, freq, units, keywords, notes, period,
+    #              g_rate, obs_vsd, vsd, r_id]], columns=md_names)
+    #         fred_md = fred_md.append(row)
+    #
+    #         row = pd.DataFrame(data=[[r_id, series_id, 'TRUE', vsd]],
+    #                            columns=fsr_names)
+    #         fsr = fsr.append(row)
+    #
+    #         cat_id = non_geo_cats[series]
+    #         row = pd.DataFrame(data=[[series_id, cat_id]], columns=cat_names)
+    #         fred_cat = fred_cat.append(row)
+    #     else:
+    #         row = pd.DataFrame(data=[[series_id, title, season, freq, units, \
+    #                                   keywords, notes, period, g_rate, obs_vsd, \
+    #                                   vsd, r_id]], columns=md_names)
+    #         geo_md = geo_md.append(row)
+    #
+    #         row = pd.DataFrame(data=[[r_id, series_id, 'TRUE', vsd]],
+    #                            columns=fsr_names)
+    #         fsr_geo = fsr_geo.append(row)
+    #
+    #     title = pd.DataFrame(data=[[title]])
+    #     titles = titles.append(title)
+    #
+    # geo_md.to_csv('fred_series_geo.txt', sep='\t', index=False)
+    # fsr_geo.to_csv('fred_series_release_geo.txt', sep='\t', index=False)
+    #
+    # fred_md.to_csv('fred_series.txt', sep='\t', index=False)
+    # fsr.to_csv('fred_series_release.txt', sep='\t', index=False)
+    # fred_cat.to_csv('fred_series_in_category.txt', sep='\t', index=False)
+    # titles.to_csv('title.txt', sep='\t', index=False, header=False)
