ba396f2599cf3616b61a1e09f6f25d9e0e63b482
Travis <travis.j.may@stls.frb.org>
2017-02-07 14:01:32 -0600

    Create metadata

--- small_business_ownership_patterns/scripts/county_biz_paterns.py
+++ small_business_ownership_patterns/scripts/county_biz_paterns.py
@@ -6,7 +6,6 @@ def multi_ordered_merge(lst_dfs):
 
     return ft.reduce(reduce_func, lst_dfs)
 
-
 def small_biz_rate(biz_ptrns,lf,date):
 
     biz_keep = 'fips|n1_4|n5_9|n10_19|n20_49|n50_99|n100_249|n250_499'
@@ -29,9 +28,9 @@ def small_biz_rate(biz_ptrns,lf,date):
     biz_df['small_biz']=biz_df.sum(axis=1)
     biz_df=biz_df.filter(regex=biz_drop,axis=1)
 
-    lf_keep='GEO.id2|GEO.display-label|HC02_EST_VC01|HC01_EST_VC01'
+    lf_keep='GEO.id2|HC02_EST_VC01|HC01_EST_VC01'
     lf_drop=['pop','lf_pt']
-    lf_names = ['fips','county','pop','lf_pt']
+    lf_names = ['fips','pop','lf_pt']
     lf_df = pd.read_csv(lf,encoding='windows-1252',skiprows={1},\
                         low_memory=False)
     lf_df=lf_df.filter(regex=lf_keep,axis=1)
@@ -48,6 +47,8 @@ def small_biz_rate(biz_ptrns,lf,date):
 
 def main():
     data_dir = os.getcwd() + '\\data\\'
+    counties = pd.read_table('..\\national_county.txt', dtype=str, sep=';')
+
     # Read in and process data
     df_09 = small_biz_rate(data_dir+'cbp09co.txt',data_dir+'laborforce_09.csv','2009')
     df_10 = small_biz_rate(data_dir+'cbp10co.txt',data_dir+'laborforce_10.csv','2010')
@@ -59,12 +60,12 @@ def main():
     dfs = [df_09,df_10,df_11,df_12,df_13,df_14]
 
     df = multi_ordered_merge(dfs)
+    df = pd.merge(df, counties, on='fips')
     df = df.sort_values(['fips','date'])
     df.fillna('.',axis=1,inplace=True)
 
     # Create output files
-    for l in pd.unique(df['fips'].ravel()):
-        series = l
+    for series in pd.unique(df['fips'].ravel()):
         frame = df[df['fips'] == series]
         series_id = 'SMLBIZOWN' + series
         frame.reset_index(inplace=True)
@@ -75,3 +76,70 @@ def main():
 
 if __name__=='__main__':
     main()
+
+#     md_names = ['series_id', 'title', 'season', 'frequency', 'units', \
+#                 'keywords', 'notes', 'period_description', 'growth_rates', \
+#                 'obs_vsd_use_release_date', 'valid_start_date', 'release_id']
+#     fsr_names = ['fred_release_id', 'fred_series_id', 'official','valid_start_date']
+#     cat_names = ['series_id', 'cat_id']
+#
+#     geo_md = pd.DataFrame(columns=md_names)
+#     fred_md = pd.DataFrame(columns=md_names)
+#     fsr_geo = pd.DataFrame(columns=fsr_names)
+#     fsr = pd.DataFrame(columns=fsr_names)
+#     fred_cat = pd.DataFrame(columns=cat_names)
+#     titles = pd.DataFrame()
+#
+#     season = 'Not Seasonally Adjusted'
+#     freq = 'Annual'
+#     units = 'Rate'
+#     keywords = ''
+#     notes = 'The small business ownership rate is the number of firms which employ fewer than 500 people, divided by the number of people in the labor force.'
+#     period = ''
+#     g_rate = 'TRUE'
+#     obs_vsd = 'TRUE'
+#     vsd = '2017-02-07'
+#     r_id = '421'
+#
+#     non_geo_fips = '002020|002110|002220|002230|002275|006075|008014|015003|042101'
+#
+#     non_geo_cats = {'002020': '27406', '002110': '27412', '002220': '27422', \
+#                     '002230': '33516', '002275': '33518', '006075': '27559', \
+#                     '008014': '32077', '015003': '27889', '042101': '29664'}
+#         title = 'Small Business Ownership Rate for ' + \
+#                 pd.unique(df[df['fips'] == series]['county'])[0]
+#
+#         # Create metadata files
+#         if bool(re.search(non_geo_fips, series)):
+#             row = pd.DataFrame(data=[
+#                 [series_id, title, season, freq, units, keywords, notes, period,
+#                  g_rate, obs_vsd, vsd, r_id]], columns=md_names)
+#             fred_md = fred_md.append(row)
+#
+#             row = pd.DataFrame(data=[[r_id, series_id, 'TRUE', vsd]],
+#                                columns=fsr_names)
+#             fsr = fsr.append(row)
+#
+#             cat_id = non_geo_cats[series]
+#             row = pd.DataFrame(data=[[series_id, cat_id]], columns=cat_names)
+#             fred_cat = fred_cat.append(row)
+#         else:
+#             row = pd.DataFrame(data=[[series_id, title, season, freq, units, \
+#                                       keywords, notes, period, g_rate, obs_vsd, \
+#                                       vsd, r_id]], columns=md_names)
+#             geo_md = geo_md.append(row)
+#
+#             row = pd.DataFrame(data=[[r_id, series_id, 'TRUE', vsd]],
+#                                columns=fsr_names)
+#             fsr_geo = fsr_geo.append(row)
+#
+#         title = pd.DataFrame(data=[[title]])
+#         titles = titles.append(title)
+#
+#     geo_md.to_csv('fred_series_geo.txt', sep='\t', index=False)
+#     fsr_geo.to_csv('fred_series_release_geo.txt', sep='\t', index=False)
+#
+#     fred_md.to_csv('fred_series.txt', sep='\t', index=False)
+#     fsr.to_csv('fred_series_release.txt', sep='\t', index=False)
+#     fred_cat.to_csv('fred_series_in_category.txt', sep='\t', index=False)
+#     titles.to_csv('title.txt', sep='\t', index=False, header=False)
