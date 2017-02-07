e11f9ff2c95eb3176dd139863681b0474e3943be
Travis <travis.j.may@stls.frb.org>
2017-02-07 11:23:40 -0600

    Create fred metadata files

--- racial_segregation/scripts/racial_seg.py
+++ racial_segregation/scripts/racial_seg.py
@@ -1,4 +1,3 @@
-        frame = df[df['fips'] == series]
 import pandas as pd, os, sys, functools as ft, pycurl as pyc, datetime as dt, re, numpy as np,math
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -46,7 +45,7 @@ def diss_index(county_file,tract_file,date):
     df=df.groupby(df.fips).sum()
 
     # Calculate racial disparity
-    df['nwt_diss'] = .5*df['nonwhite_white']
+    df['nwt_diss'] = (.5*df['nonwhite_white'])*100
     # df['blk_diss'] = .5*df['black_white']
     # df['asn_diss'] = .5*df['asian_white']
     # df['esp_diss'] = .5*df['hispanic_white']
@@ -82,6 +81,37 @@ def main():
     df = df.sort_values(['fips','date'])
     df.fillna('.',axis=1,inplace=True)
 
+    md_names = ['series_id', 'title', 'season', 'frequency', 'units', \
+                'keywords', 'notes', 'period_description', 'growth_rates', \
+                'obs_vsd_use_release_date', 'valid_start_date', 'release_id']
+    fsr_names = ['fred_release_id', 'fred_series_id', 'official',
+                 'valid_start_date']
+    cat_names = ['series_id', 'cat_id']
+
+    geo_md = pd.DataFrame(columns=md_names)
+    fred_md = pd.DataFrame(columns=md_names)
+    fsr_geo = pd.DataFrame(columns=fsr_names)
+    fsr = pd.DataFrame(columns=fsr_names)
+    fred_cat = pd.DataFrame(columns=cat_names)
+    titles = pd.DataFrame()
+
+    season = 'Not Seasonally Adjusted'
+    freq = 'Annual'
+    units = 'Percent'
+    keywords = ''
+    notes = 'The Racial Dissimilarity Index measures the percentage of the non-hispanic white population in a county which would have to change Census tracts to equalize the racial distribution between white and non-white population groups across all tracts in the county.'
+    period = ''
+    g_rate = 'TRUE'
+    obs_vsd = 'TRUE'
+    vsd = '2017-02-07'
+    r_id = '419'
+
+    non_geo_fips = '002020|002110|002220|002230|002275|006075|008014|015003|042101'
+
+    non_geo_cats = {'002020': '27406', '002110': '27412', '002220': '27422', \
+                    '002230': '33516', '002275': '33518', '006075': '27559', \
+                    '008014': '32077', '015003': '27889', '042101': '29664'}
+
     for series in pd.unique(df['fips'].ravel()):
         series_id = 'RACEDISPARITY' + series
         frame = df[df['fips'] == series]
@@ -90,6 +120,44 @@ def main():
         frame.set_index('date', inplace=True)
         frame.columns = [series_id]
         frame.to_csv('output\\' + series_id, sep='\t')
+
+        title = 'White to Non-White Racial Dissimilarity Index for ' + \
+                pd.unique(df[df['fips'] == series]['county'])[0]
+
+        # Create metadata files
+        if bool(re.search(non_geo_fips, series)):
+            row = pd.DataFrame(data=[
+                [series_id, title, season, freq, units, keywords, notes, period,
+                 g_rate, obs_vsd, vsd, r_id]], columns=md_names)
+            fred_md = fred_md.append(row)
+
+            row = pd.DataFrame(data=[[r_id, series_id, 'TRUE', vsd]],
+                               columns=fsr_names)
+            fsr = fsr.append(row)
+
+            cat_id = non_geo_cats[series]
+            row = pd.DataFrame(data=[[series_id, cat_id]], columns=cat_names)
+            fred_cat = fred_cat.append(row)
+        else:
+            row = pd.DataFrame(data=[[series_id, title, season, freq, units, \
+                                      keywords, notes, period, g_rate, obs_vsd, \
+                                      vsd, r_id]], columns=md_names)
+            geo_md = geo_md.append(row)
+
+            row = pd.DataFrame(data=[[r_id, series_id, 'TRUE', vsd]],
+                               columns=fsr_names)
+            fsr_geo = fsr_geo.append(row)
+
+        title = pd.DataFrame(data=[[title]])
+        titles = titles.append(title)
+
+    geo_md.to_csv('fred_series_geo.txt', sep='\t', index=False)
+    fsr_geo.to_csv('fred_series_release_geo.txt', sep='\t', index=False)
+
+    fred_md.to_csv('fred_series.txt', sep='\t', index=False)
+    fsr.to_csv('fred_series_release.txt', sep='\t', index=False)
+    fred_cat.to_csv('fred_series_in_category.txt', sep='\t', index=False)
+    titles.to_csv('title.txt', sep='\t', index=False, header=False)
 
 if __name__=='__main__':
     main()
