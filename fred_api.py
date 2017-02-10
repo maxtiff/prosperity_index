a09eb6cc8edc92ff641e65e78a32fecd4dcbe947
Travis <travis.j.may@stls.frb.org>
2017-02-10 15:55:46 -0600

    Add api calls

--- fred_api.py
+++ fred_api.py
@@ -10,7 +10,29 @@ data_dir = os.getcwd()+'\\data\\'
 base_url = 'https://api.stlouisfed.org/'
 key_param = 'api_key='
 api_key = '7945c845bf4a1f2f7293f551db41d5df'
+'''
 
+Take release_ids -> 148,330,399,403
+https://api.stlouisfed.org/fred/release/series?release_id=330'\
+&api_key=7945c845bf4a1f2f7293f551db41d5df&tag_names=prosperity%20scorecard&limit=1
+
+Take tag (prosperity scorecard)
+
+Grab series ids (one from each release)
+
+Then ->
+'https://api.stlouisfed.org/geofred/series/group?series_id=netmignacs001001'\
+'&api_key=7945c845bf4a1f2f7293f551db41d5df&file_type=json'
+
+Grab min and max dates
+
+Then->
+'https://api.stlouisfed.org/geofred/series/data?series_id=netmignacs001001'\
+'&api_key=7945c845bf4a1f2f7293f551db41d5df&file_type=json&date=2013-01-01'\
+'&start_date=2009-01-01'
+
+Grab dates and values a
+'''
 county = 'co'
 short_year = dt.datetime.today().strftime('%y')
 long_year = dt.datetime.today().year
