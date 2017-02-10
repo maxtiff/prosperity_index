9181accc740590be753687969a0cab815ff9317a
Travis <travis.j.may@stls.frb.org>
2017-02-10 17:18:33 -0600

    add urls

--- fred_api.py
+++ fred_api.py
@@ -6,21 +6,23 @@ def open_loc(file_loc,file_url):
         c.perform()
         c.close()
 
+import requests as req, json
+
 data_dir = os.getcwd()+'\\data\\'
 base_url = 'https://api.stlouisfed.org/'
 fred_dir=['fred/','geofred/']
-type_dir=['series/','release/']
+type_dir=['release/','series/']
 query_start=['series?','group?','data?']
-key_param = 'api_key='
+api_key_param = 'api_key='
 series_param='series_id='
 release_param='release_id='
 file_type='file_type=json'
 tag_param='tag_names='
 tags = ['prosperity%20scorecard']
+limit_param = 'limit=1'
 date_params=['date','start_date']
 release_ids=['148','330','399','406','408','409','410','412','413','414','415','416','417','419','421']
-series_ids=[] # populate with calls to releases
-
+series_ids=[] # populate with get response from release calls
 
 api_key = '7945c845bf4a1f2f7293f551db41d5df'
 '''
@@ -47,11 +49,28 @@ Read through to grab dates and values
 '''
 
 # Get data
-filename = county + year + month + type[0] + '.' + ext
-full_url = base_url + filename
-with open(data_dir + filename, 'wb') as f:
-    c = pyc.Curl()
-    c.setopt(c.URL, full_url)
-    c.setopt(c.WRITEDATA, f)
-    c.perform()
-    c.close()
+release_url = base_url + fred_dir[0] + type_dir[0] + query_start[0] + release_param \
+    + release_ids[0] + '&' + api_key_param + api_key + '&' + tag_param \
+    + tags[0] + '&' + file_type + '&' + limit_param
+
+r = req.get(release_url)
+r.json()
+id = list(r.json().values())[5][0]['id']
+
+series_ids.append(id)
+
+series_url = base_url + fred_dir[1] + type_dir[0] + query_start[1] + series_param \
+    + series_ids[0] + '&' + api_key_param + api_key + '&' + file_type
+
+s = req.get(series_url)
+s.json()
+dates ={}
+dates['date'] = list(s.json().values())[0][0]['max_date']
+dates['start_date'] = list(s.json().values())[0][0]['min_date']
+
+obs_url = base_url + fred_dir[1] + type_dir[1] + query_start[2] + series_param \
+    + series_ids[0] + '&' + api_key_param + api_key + '&' + file_type + '&' \
+    + date_params[0] + list(dates.values())[0] + '&' + date_params[1] \
+    + list(dates.values())[1]
+
+obs = req.get(obs_url)
