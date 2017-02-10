2d8107588ce65094c51dc3db36903bdb99d3eb20
Travis <travis.j.may@stls.frb.org>
2017-02-10 16:22:43 -0600

    create api vars

--- fred_api.py
+++ fred_api.py
@@ -8,66 +8,50 @@ def open_loc(file_loc,file_url):
 
 data_dir = os.getcwd()+'\\data\\'
 base_url = 'https://api.stlouisfed.org/'
+fred_dir=['fred/','geofred/']
+type_dir=['series/','release/']
+query_start=['series?','group?','data?']
 key_param = 'api_key='
+series_param='series_id='
+release_param='release_id='
+file_type='file_type=json'
+tag_param='tag_names='
+tags = ['prosperity%20scorecard']
+date_params=['date','start_date']
+release_ids=['148','330','399','406','408','409','410','412','413','414','415','416','417','419','421']
+series_ids=[] # populate with calls to releases
+
+
 api_key = '7945c845bf4a1f2f7293f551db41d5df'
 '''
 
-Take release_ids -> 148,330,399,403
-https://api.stlouisfed.org/fred/release/series?release_id=330'\
+Take release_ids -> 148,330,399,406,408,409,410,412,413,414,415,416,417,419,421
+
+https://api.stlouisfed.org/fred/release/series?release_id=RELEASE_ID_HERE'\
 &api_key=7945c845bf4a1f2f7293f551db41d5df&tag_names=prosperity%20scorecard&limit=1
 
-Take tag (prosperity scorecard)
+Grab series ids (one from each release) from returned item
 
-Grab series ids (one from each release)
-
-Then ->
-'https://api.stlouisfed.org/geofred/series/group?series_id=netmignacs001001'\
+Then insert series_id into ->
+'https://api.stlouisfed.org/geofred/series/group?series_id=SERIES_ID_HERE'\
 '&api_key=7945c845bf4a1f2f7293f551db41d5df&file_type=json'
 
 Grab min and max dates
 
 Then->
-'https://api.stlouisfed.org/geofred/series/data?series_id=netmignacs001001'\
-'&api_key=7945c845bf4a1f2f7293f551db41d5df&file_type=json&date=2013-01-01'\
-'&start_date=2009-01-01'
+'https://api.stlouisfed.org/geofred/series/data?series_id=SERIES_ID_HERE'\
+'&api_key=7945c845bf4a1f2f7293f551db41d5df&file_type=json&date=MAX_DATE'\
+'&start_date=MIN_DATE'
 
-Grab dates and values a
+Read through to grab dates and values
 '''
-county = 'co'
-short_year = dt.datetime.today().strftime('%y')
-long_year = dt.datetime.today().year
-type = ['c','a']
-ext = 'txt'
 
 # Get data
-for y in range(int(short_year)):
-    if y >= 10:
-        year = (str(y))
-    else:
-        year = ('0' + str(y))
-    for m in range(1, 13):
-        if m >= 10:
-            month = (str(m))
-        else:
-            month = ('0'+str(m))
-        filename = county + year + month + type[0] + '.' + ext
-        full_url = base_url + filename
-
-        with open(data_dir + filename, 'wb') as f:
-            c = pyc.Curl()
-            c.setopt(c.URL, full_url)
-            c.setopt(c.WRITEDATA, f)
-            c.perform()
-            c.close()
-
-for l in range(1990,long_year-1):
-    year = str(l)
-    filename = county + year + type[1] + '.' + ext
-    full_url = base_url + filename
-
-    with open(data_dir + filename, 'wb') as f:
-        c = pyc.Curl()
-        c.setopt(c.URL, full_url)
-        c.setopt(c.WRITEDATA, f)
-        c.perform()
-        c.close()
+filename = county + year + month + type[0] + '.' + ext
+full_url = base_url + filename
+with open(data_dir + filename, 'wb') as f:
+    c = pyc.Curl()
+    c.setopt(c.URL, full_url)
+    c.setopt(c.WRITEDATA, f)
+    c.perform()
+    c.close()
