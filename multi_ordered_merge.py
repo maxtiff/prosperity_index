e58fa5bff3409a8bbb728d69f67b402f26f3db42
Travis <travis.j.may@stls.frb.org>
2017-02-14 15:18:31 -0600

    fix pandas import

--- multi_ordered_merge.py
+++ multi_ordered_merge.py
@@ -1,6 +1,7 @@
-import functools as ft,pandas as pd
+import functools as ft, pandas
 
 def multi_ordered_merge(lst_dfs):
-    reduce_func = lambda left,right: pd.ordered_merge(left, right)
+
+    reduce_func = lambda left,right: pandas.ordered_merge(left, right)
 
     return ft.reduce(reduce_func, lst_dfs)
