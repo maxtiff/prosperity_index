89ce46c4ee91a6222f2ba658b65b47a691645b7c
Travis <travis.j.may@stls.frb.org>
2017-05-09 09:11:30 -0500

    Modify series ids

--- racial_population/scripts/racial_population.py
+++ racial_population/scripts/racial_population.py
@@ -1,13 +1,18 @@
-import pandas as pd, os,multi_ordered_merge as merger
+import pandas as pd, os,multi_ordered_merge as merger,functools as ft
 pd.options.mode.chained_assignment = None  # default='warn'
 
-payload = {'get': ['NAME,B03002_003E'], 'for': 'county:*','key': 'abeed2f8242a2f5fc4f5b8c1ff0ad4a49dd1b76a'}
+# payload = {'get': ['NAME,B03002_003E'], 'for': 'county:*','key': 'abeed2f8242a2f5fc4f5b8c1ff0ad4a49dd1b76a'}
 
-def make_request(url,payload):
+# def make_request(url,payload):
+#
+#     r = req.get(url,params=payload,stream=True)
+#
+#     return r.json()
 
-    r = req.get(url,params=payload,stream=True)
-
-    return r.json()
+# demos = ['B03002_003E','B03002_004E','B03002_005E','B03002_006E','B03002_012E']
+#
+# payload = {'get': ['NAME',demos[n]], 'for': 'county:*','key': 'abeed2f8242a2f5fc4f5b8c1ff0ad4a49dd1b76a'}
+# r = req.get('http://api.census.gov/data/2009/acs5',params=payload)
 
 def racial_pop(county_file,date):
     keep_header = ['GEO.id2','HD01_VD03','HD01_VD04','HD01_VD05','HD01_VD06','HD01_VD12']
@@ -49,8 +54,9 @@ def main():
         frame = df[df['fips'] == series]
         frame.reset_index(inplace=True)
         frame.drop(['index'], axis=1, inplace=True)
+        # for c in demos:
         for c in ['HD01_VD03','HD01_VD04','HD01_VD05','HD01_VD06','HD01_VD12']:
-            series_id = 'B03002' + frame[c].name[-4:] + series
+            series_id = 'B030020' + frame[c].name[-2:] + 'E' + series
             output = frame[['date',c]]
             output.set_index('date', inplace=True)
             output.columns = [series_id]
