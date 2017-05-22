a544448d91c70af9191b517f02a5c8d663f6f54a
Travis <travis.j.may@stls.frb.org>
2017-05-22 14:32:23 -0500

    Add arguments parsing for census id and year

--- median_age/scripts/CountyMedianAge.py
+++ median_age/scripts/CountyMedianAge.py
@@ -1,4 +1,4 @@
-import pandas as pd, requests as req, os
+import pandas as pd, requests as req, os, argparse
 pd.options.mode.chained_assignment = None  # default='warn'
 
 def make_request(url,payload):
@@ -27,7 +27,7 @@ def build_df_from_api(api_id,date,key):
     return frame
 
 def output_series_files(df,api_id,date):
-    directory = 'output_test'
+    directory = 'output'
 
     try:
         os.stat(directory)
@@ -35,12 +35,12 @@ def output_series_files(df,api_id,date):
         os.mkdir(directory)
 
     for series in pd.unique(df['fips'].ravel()):
+        series_id = api_id + series
         frame = df[df['fips'] == series]
         frame.reset_index(inplace=True)
         frame.drop(['index'], axis=1, inplace=True)
-        series_id = api_id + series
         frame['date'] = date
-        output = frame[['date', c]]
+        output = frame[['date', series]]
         output.set_index('date', inplace=True)
         output.columns = [series_id]
         output.to_csv(os.path.join(directory,series_id), sep='\t')
@@ -48,16 +48,15 @@ def output_series_files(df,api_id,date):
     pass
 def main():
 
-    # demos = cf.DEMOGRPAHICS
-    # date = cf.DATE
-    # api_key = cf.API_KEY
+    parser = argparse.ArgumentParser()
+    parser.add_argument('id',help='This would be the Census code for the indicator that you want to retrieve.')
+    parser.add_argument('year',help='This would be the observation year for the data that you want to retrieve')
+    args = parser.parse_args()
 
-    api_id='B01002_001E'
-    date='2015'
     api_key = 'abeed2f8242a2f5fc4f5b8c1ff0ad4a49dd1b76a'
-    racial_population = build_df_from_api(api_id,date,api_key)
-    output_series_files(racial_population,api_id,date)
 
+    racial_population = build_df_from_api(args.id,args.id,api_key)
+    output_series_files(racial_population,args.id,args.id)
 
 if __name__ == '__main__':
     main()
