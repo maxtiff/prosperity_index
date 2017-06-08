3b39db941c6bbe886b2111624794e545ccfe2474
Travis <travis.j.may@stls.frb.org>
2017-06-08 16:10:48 -0500

    create fips list for metadata; clean-up

--- income_segregation/inc_seg_output.py
+++ income_segregation/inc_seg_output.py
@@ -38,34 +38,3 @@ def multi_merge(lst_dfs):
 
 if __name__=='__main__':
     main()
-
-
-# Test section
-# output_dir = os.path.join(os.getcwd(),'output')
-# files = os.listdir(output_dir)
-#
-# counties = pd.read_table('..\\national_county.txt', dtype=str, sep=';')
-# states = pd.read_table('..\\state_fips.txt')
-#
-# df = pd.DataFrame()
-# df_lst = []
-#
-# ptn = '\w{5}AK\d{4}'
-#
-# regex = re.compile(ptn)
-#
-# selected_files = list(filter(regex.search, files))
-#
-# for f in selected_files:
-#     wf = pd.read_table(os.path.join(output_dir, f), sep='\t', dtype='str')
-#     df_lst.append(wf)
-#
-# frame = multi_merge(df_lst)
-#
-# for i in range(len(frame)):
-#     fips = '0{}'.format(frame.iloc[i][0])
-#     id = 'INCSEG{}'.format(fips)
-#     series = pd.DataFrame(frame.iloc[i].drop('COUNTY')).reset_index()
-#     series.columns = ['date',id]
-#     series.to_csv(os.path.join(output_dir, id), sep='\t',index=False)
-
